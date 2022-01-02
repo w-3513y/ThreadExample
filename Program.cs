8e1917038a1354ec1860a436bcd6afb9464565a9
Wesley <wesleyferreirasb@gmail.com>
2022-01-01 21:56:39 -0300

    commit final

--- Program.cs
+++ Program.cs
@@ -5,28 +5,28 @@ namespace ThreadExample
 {
     class Program
     {
-        static readonly object _object = new object();  
-  
-        static void TestLock()  
-        {  
-              
-            lock (_object)  
-            {  
-                Thread.Sleep(100);  
+        static readonly object _object = new object();
+
+        static void TestLock()
+        {
+
+            lock (_object)
+            {
+                Thread.Sleep(100);
                 Console.WriteLine(Environment.TickCount);
-            }  
-        }  
-  
-        static void Main(string[] args)      
-        {  
-            for (int i = 0; i < 10; i++)  
-            {  
-                ThreadStart start = new ThreadStart(TestLock);  
-                new Thread(start).Start();  
-            }  
-  
-            Console.ReadLine();  
-        }  
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadStart start = new(TestLock);
+                new Thread(start).Start();
+            }
+
+            Console.ReadLine();
+        }
 
     }
 }
