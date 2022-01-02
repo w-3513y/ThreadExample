bbeb8e9ca8a9af755b61dc2188476df708c76185
Wesley <wesleyferreirasb@gmail.com>
2022-01-01 21:29:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadExample
+{
+    class Program
+    {
+        static readonly object _object = new object();  
+  
+        static void TestLock()  
+        {  
+              
+            lock (_object)  
+            {  
+                Thread.Sleep(100);  
+                Console.WriteLine(Environment.TickCount);
+            }  
+        }  
+  
+        static void Main(string[] args)      
+        {  
+            for (int i = 0; i < 10; i++)  
+            {  
+                ThreadStart start = new ThreadStart(TestLock);  
+                new Thread(start).Start();  
+            }  
+  
+            Console.ReadLine();  
+        }  
+
+    }
+}
